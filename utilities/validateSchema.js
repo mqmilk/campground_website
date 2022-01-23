71c9a83a393d1119b0ed6d3e1a61003fd063388e
mqmilk <50921277+mqmilk@users.noreply.github.com>
2022-01-23 16:59:12 -0500

    Add files via upload

--- utilities/validateSchema.js
+++ utilities/validateSchema.js
@@ -1,11 +1,35 @@
-const Joi = require("joi");
+const BasicJoi = require("joi");
+const sanitizeHtml = require("sanitize-html");
+
+const extension = (joi) => ({
+
+    type: 'string',
+    base: joi.string(),
+    messages: {
+        'string.escapeHTML': 'Should not contain any html tags.',
+    },
+    rules: {
+        escapeHTML: {
+            validate(value, helpers) {
+                const clean = sanitizeHtml(value, {
+                    allowedTags: [],
+                    allowedAttributes: {},
+                });
+                if (clean !== value) return helpers.error('string.escapeHTML', { value })
+                return clean;
+            }
+        }
+    }
+  });
+
+const Joi = BasicJoi.extend(extension);
 
 //validate schema for Campground model
 module.exports.schemaCamp = Joi.object({
-    title: Joi.string().required(),
-    location: Joi.string().required(),
+    title: Joi.string().required().escapeHTML(),
+    location: Joi.string().required().escapeHTML(),
     price: Joi.number().required().min(0),
-    description: Joi.string().required(),
+    description: Joi.string().required().escapeHTML(),
     deleteImages: Joi.array(),
 });
 
@@ -13,5 +37,5 @@ module.exports.schemaCamp = Joi.object({
 //validate schema for Review model
 module.exports.schemaReview = Joi.object({
     rating: Joi.number().required().min(0).max(5),
-    body: Joi.string().required(),
+    body: Joi.string().required().escapeHTML(),
 });
